d1c57917e9e1d527730c88b9343e219d9519f7ef
Diluka Athukorala <dilukaathukorala@gmail.com>
2025-11-29 19:45:51 +0530

    Import java libraries

--- java-calculator/src/com/dilu/calculator/CalculatorApp.java
+++ java-calculator/src/com/dilu/calculator/CalculatorApp.java
@@ -1,14 +1,6 @@
 package com.dilu.calculator;
 
-public class CalculatorApp {
-    public static void main(String[] args){
+import javax.swing.*;
+import java.awt.*;
 
-       for(int i=0; i<=10; i++){
-           System.out.print(" "+ i +" ");
-           if(i==5){
-               System.out.println("");
-               System.out.println("First sector is over");
-           }
-       }
-    }
-}
+
