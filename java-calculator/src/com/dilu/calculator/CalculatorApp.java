573f3f90f439a8792819fa4f993bf63a0f1bebd6
Diluka Athukorala <dilukaathukorala@gmail.com>
2025-11-29 20:40:45 +0530

    Defined variables to store calculations

--- java-calculator/src/com/dilu/calculator/CalculatorApp.java
+++ java-calculator/src/com/dilu/calculator/CalculatorApp.java
@@ -3,4 +3,12 @@ package com.dilu.calculator;
 import javax.swing.*;
 import java.awt.*;
 
+public class CalculatorApp extends JFrame{
 
+    private JTextField display;
+    private double num1=0;
+    private String operator = "";
+    private boolean startNew = true;
+
+
+}
