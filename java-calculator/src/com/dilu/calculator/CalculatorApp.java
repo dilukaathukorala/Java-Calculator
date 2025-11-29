46a5ac0d0deaebe6ad20943bdfe36fc391962e9c
Diluka Athukorala <dilukaathukorala@gmail.com>
2025-11-29 21:07:14 +0530

    Implemented display

--- java-calculator/src/com/dilu/calculator/CalculatorApp.java
+++ java-calculator/src/com/dilu/calculator/CalculatorApp.java
@@ -10,5 +10,13 @@ public class CalculatorApp extends JFrame{
     private String operator = "";
     private boolean startNew = true;
 
+    public CalculatorApp(){
+
+        setTitle("Calculator");
+        setSize(300, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        }
 
 }
