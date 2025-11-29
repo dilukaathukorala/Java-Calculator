b403d0c0b5c4179390fa2a8008bf85b3463311af
Diluka Athukorala <dilukaathukorala@gmail.com>
2025-11-29 18:06:45 +0530

    Tested code
    Success

--- java-calculator/src/com/dilu/calculator/CalculatorApp.java
+++ java-calculator/src/com/dilu/calculator/CalculatorApp.java
@@ -2,6 +2,13 @@ package com.dilu.calculator;
 
 public class CalculatorApp {
     public static void main(String[] args){
-        System.out.println("Calculator project started!");
+
+       for(int i=0; i<=10; i++){
+           System.out.print(" "+ i +" ");
+           if(i==5){
+               System.out.println("");
+               System.out.println("First sector is over");
+           }
+       }
     }
 }
