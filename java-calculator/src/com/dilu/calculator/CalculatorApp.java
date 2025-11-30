cfb466908bf326713f5805d4e282356ed3396f56
Diluka Athukorala <dilukaathukorala@gmail.com>
2025-11-30 07:07:59 +0530

    Add components to window

--- java-calculator/src/com/dilu/calculator/CalculatorApp.java
+++ java-calculator/src/com/dilu/calculator/CalculatorApp.java
@@ -39,7 +39,8 @@ public class CalculatorApp extends JFrame{
             panel.add(button);
         }
 
-
+        add(display, BorderLayout.NORTH);
+        add(panel, BorderLayout.CENTER);
     }
 
 }
